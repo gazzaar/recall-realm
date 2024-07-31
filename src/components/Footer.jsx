d8a594d5a7755230d5922509de52717b496c4732
fathyElgazar <gazzarDev@gmail.com>
2024-07-31 10:51:28 +0300

    Build footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <>
+      <p className='text-[#BCF1DB]  font-sans text-3xl mt-6 bg-[#272C36]/60 inline-block w-auto'>
+        Don&apos;t click on a card more than once!
+      </p>
+    </>
+  );
+};
+export default Footer;
